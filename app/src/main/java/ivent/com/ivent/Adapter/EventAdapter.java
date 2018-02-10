c5d1c7aa736676e786ef10979fccce87ca450050
Gal Malachi <gal1419@gmail.com>
2018-02-10 16:38:23 +0200

    event activity

---
+++ app/src/main/java/ivent/com/ivent/Adapter/EventAdapter.java
@@ -0,0 +1,115 @@
+package ivent.com.ivent.Adapter;
+
+/**
+ * Created by galmalachi on 10/02/2018.
+ */
+
+import android.content.Context;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import butterknife.BindView;
+import ivent.com.ivent.R;
+import ivent.com.ivent.model.Event;
+
+
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<Event> eventList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.title) TextView title;
+        @BindView(R.id.count) TextView count;
+        @BindView(R.id.thumbnail) ImageView thumbnail;
+        @BindView(R.id.overflow) ImageView overflow;
+
+        public MyViewHolder(View view) {
+            super(view);
+        }
+    }
+
+
+    public EventAdapter(Context mContext, List<Event> eventList) {
+        this.mContext = mContext;
+        this.eventList = eventList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.event_layout, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        Event event = eventList.get(position);
+        holder.title.setText(event.getTitle());
+        holder.count.setText(event.getParticipants().size() + " participants");
+
+        // loading event cover using Glide library
+        //Glide.with(mContext).load(event.get()).into(holder.thumbnail);
+
+        holder.overflow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(holder.overflow);
+            }
+        });
+    }
+
+    /**
+     * Showing popup menu when tapping on 3 dots
+     */
+    private void showPopupMenu(View view) {
+        // inflate menu
+        PopupMenu popup = new PopupMenu(mContext, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.manu_event, popup.getMenu());
+        popup.setOnMenuItemClickListener(new MyMenuItemClickListener());
+        popup.show();
+    }
+
+    /**
+     * Click listener for popup menu items
+     */
+    class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
+
+        public MyMenuItemClickListener() {
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.action_add_favourite:
+                    Toast.makeText(mContext, "Add to favourite", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.action_play_next:
+                    Toast.makeText(mContext, "Play next", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return eventList.size();
+    }
+}
---
+++ app/src/main/java/ivent/com/ivent/model/Event.java
@@ -0,0 +1,92 @@
+package ivent.com.ivent.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by galmalachi on 10/02/2018.
+ */
+
+public class Event {
+
+    @SerializedName("id")
+    private Long id;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("address")
+    private String address;
+
+    @SerializedName("owner")
+    private User owner;
+
+    @SerializedName("participants")
+    private List<User> participants;
+
+    @SerializedName("thumbnail")
+    private String thumbnail;
+
+
+    public Event() {
+
+    }
+
+    public Event(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public List<User> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<User> participants) {
+        this.participants = participants;
+    }
+
+    public void addParticipant(User user) {
+        this.participants.add(user);
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
+
